bbd2c964348b38a0166690fe67bfce0f8cd284aa
Piotr Paruch <piotr.paruch@visma.com>
2023-05-16 00:08:59 +0200

    update book loan job query in commented line aswell

--- app/jobs/due_date_notifications_job.rb
+++ app/jobs/due_date_notifications_job.rb
@@ -3,7 +3,7 @@ class DueDateNotificationsJob
 
   def perform
     # for notification testing purposes
-    # BookLoan.where(due_date: Time.current..Time.current + 4.minutes).each do |book_loan|
+    # BookLoan.where(status: 'checked_out', due_date: Time.current..Time.current + 4.minutes).each do |book_loan|
     BookLoan.where(status: 'checked_out', due_date: Date.tomorrow).each do |book_loan|
       UserMailer.notification_email(book_loan).deliver_later
     end
