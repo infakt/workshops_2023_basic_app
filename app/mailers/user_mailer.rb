a0da8e89cc07fc799ec0a5303d7d8f3de4b7c2da
Piotr Paruch <piotr.paruch@visma.com>
2023-10-23 13:51:23 +0200

    fix indentation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,7 +13,7 @@ class UserMailer < ApplicationMailer
     mail(to: book_loan.user.email, subject: 'Your book loan period ends tomorrow')
   end
 
-    def due_date_notification_cron_email(book_loan)
+  def due_date_notification_cron_email(book_loan)
     @book_title = book_loan.book.title
     @due_date = book_loan.due_date
 
