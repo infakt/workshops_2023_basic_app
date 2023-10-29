47b93291fb87eb45730c50a16ebb143bf73f702d
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-10-29 18:54:29 +0100

    update

--- app/services/publishers/loan_book_publisher.rb
+++ app/services/publishers/loan_book_publisher.rb
@@ -1,9 +1,12 @@
-class LoanBookPublisher
-  def initialize(message_content)
-    @message_content = message_content
-  end
 
-  def publish
-    Application.new(key: 'book_loans_app', name: 'book_loander', message: @message_content).perform
+module Publishers
+  class LoanBookPublisher
+    def initialize(message_content)
+      @message_content = message_content
+    end
+
+    def publish
+      ::Publishers::Application.new(key: 'book_loans_app', name: 'book_loaner', message_content: @message_content).perform
+    end
   end
 end
