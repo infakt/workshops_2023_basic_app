18fc901eabbf12f28767089df7b88de788a4b3aa
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-11-08 20:31:48 +0100

    fix book_loans_controller so it makes more sense

--- app/controllers/book_loans_controller.rb
+++ app/controllers/book_loans_controller.rb
@@ -7,12 +7,12 @@ class BookLoansController < ApplicationController
       if @book_loan.save
         format.html { redirect_to book_url(book), notice: flash_notice }
         format.json { render :show, status: :created, location: @book_loan }
+        notice_calendar
       else
         format.html { redirect_to book_url(book), alert: @book_loan.errors.full_messages.join(', ') }
         format.json { render json: @book_loan.errors, status: :unprocessable_entity }
       end
     end
-    notice_calendar
   end
 
   def cancel
@@ -24,13 +24,13 @@ class BookLoansController < ApplicationController
     end
   end
 
-  def notice_calendar
-    UserCalendarNotifier.new(current_user, book).insert_event
-  end
-
   private
 
   delegate :book, to: :@book_loan
+
+  def notice_calendar
+    UserCalendarNotifier.new(current_user, book).insert_event
+  end
 
   def prepare_book_loan
     @book_loan = current_user.book_loans.new(book_id: book_loan_params, due_date: Time.zone.today + 14.days)
